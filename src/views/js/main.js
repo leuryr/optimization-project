4d63974a87902b4a0449144499dd2021c2f1079f
Leury Rodriguez <leuryr@yahoo.com>
2016-04-27 18:31:57 -0400

    Move the cause of forced reflow into a variable outside of for loop.

--- src/views/js/main.js
+++ src/views/js/main.js
@@ -489,9 +489,10 @@ function updatePositions() {
   window.performance.mark("mark_start_frame");
 
   var items = document.getElementsByClassName('mover');
+  var value = document.body.scrollTop / 1250;
 
   for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+    var phase = Math.sin((value) + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
